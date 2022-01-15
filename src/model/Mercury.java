ddc57b728f58ba1f62ee02dc6184bc7cbf3eae00
Linus Moreau <linus.moreau@gmail.com>
2022-01-15 12:31:11 -0800

    Set up planets

---
+++ src/model/Mercury.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Mercury {
+    private long resources;
+    private long equipment;
+    private long mechanics;
+    private long excavators;
+    private long manufactures;
+    private long solarPanels;
+    private long launchers;
+
+    public Mercury() {
+        resources = 0;
+        equipment = 0;
+        mechanics = 0;
+        excavators = 0;
+        manufactures = 0;
+        solarPanels = 0;
+        launchers = 0;
+    }
+
+
+}
