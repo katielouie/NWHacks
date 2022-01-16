f1bd418b587ba814d1f73e21a2db5454831fc1af
Linus Moreau <linus.moreau@gmail.com>
2022-01-15 17:37:11 -0800

    Initialize Mercury infrastructure

---
+++ src/model/Infrastructure.java
@@ -0,0 +1,140 @@
+package model;
+
+import java.util.Random;
+
+public abstract class Infrastructure {
+    private final Random randomGenerator;
+
+    protected Long maxActive;     // null if no max
+    protected long active;
+    protected long inactive;
+    protected long broken;
+
+    // Equipment and material costs to build one unit
+    private final int resourceBuildCost;
+    private final int equipmentBuildCost;
+
+    // Chance per million per day to breakdown
+    private final int activeBreakdownChance;
+    private final int inactiveBreakdownChance;
+
+    // Equipment and material costs to repair one unit
+    private final int materialRepairCost;
+    private final int equipmentRepairCost;
+
+    // Energy use per unit
+    private final int activeEnergyUse;
+    private final int inactiveEnergyUse;
+
+    public Infrastructure(int resourceBuildCost, int equipmentBuildCost,
+                          int activeBreakdownChance, int inactiveBreakdownChance,
+                          int materialRepairCost, int equipmentRepairCost,
+                          int activeEnergyUse, int inactiveEnergyUse) {
+        maxActive = null;
+        active = 0;
+        inactive = 0;
+        broken = 0;
+        this.resourceBuildCost = resourceBuildCost;
+        this.equipmentBuildCost = equipmentBuildCost;
+        this.activeBreakdownChance = activeBreakdownChance;
+        this.inactiveBreakdownChance = inactiveBreakdownChance;
+        this.materialRepairCost = materialRepairCost;
+        this.equipmentRepairCost = equipmentRepairCost;
+        this.activeEnergyUse = activeEnergyUse;
+        this.inactiveEnergyUse = inactiveEnergyUse;
+        randomGenerator = new Random();
+    }
+
+    public void deteriorate() {
+        deteriorateActive();
+        deteriorateInactive();
+    }
+    
+    private void deteriorateActive() {
+        double z = randomGenerator.nextGaussian();
+        double p = activeBreakdownChance / 1_000_000.0;
+        double sd = Math.sqrt(active * p * (1 - p));
+        double amount = active * p + sd * z;
+        if (amount < 0) {
+            amount = 0;
+        } else if (amount > active) {
+            amount = active;
+        }
+        broken += amount;
+        active -= amount;
+    }
+    
+    private void deteriorateInactive() {
+        double z = randomGenerator.nextGaussian();
+        double p = inactiveBreakdownChance / 1_000_000.0;
+        double sd = Math.sqrt(inactive * p * (1 - p));
+        double amount = inactive * p + sd * z;
+        if (amount < 0) {
+            amount = 0;
+        } else if (amount > inactive) {
+            amount = inactive;
+        }
+        broken += amount;
+        inactive -= amount;
+    }
+
+    public void build(long num) throws IllegalArgumentException {
+        Mercury mercury = Mercury.mercury;
+        long usedResources = resourceBuildCost * num;
+        long usedEquipment = equipmentBuildCost * num;
+        if (usedResources > mercury.getResources() || usedEquipment > mercury.getEquipment()) {
+            throw new IllegalArgumentException("Insufficient resources");
+        } else {
+            mercury.setResources(mercury.getResources() - usedResources);
+            mercury.setEquipment(mercury.getEquipment() - usedEquipment);
+        }
+    }
+
+    public void setMaxActive(Long maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public Long getMaxActive() {
+        return maxActive;
+    }
+
+    public long getWorking() {
+        return inactive + active;
+    }
+
+    public long getBroken() {
+        return broken;
+    }
+
+    public int getResourceBuildCost() {
+        return resourceBuildCost;
+    }
+
+    public int getEquipmentBuildCost() {
+        return equipmentBuildCost;
+    }
+
+    public int getActiveBreakdownChance() {
+        return activeBreakdownChance;
+    }
+
+    public int getInactiveBreakdownChance() {
+        return inactiveBreakdownChance;
+    }
+
+    public int getMaterialRepairCost() {
+        return materialRepairCost;
+    }
+
+    public int getEquipmentRepairCost() {
+        return equipmentRepairCost;
+    }
+
+    public int getActiveEnergyUse() {
+        return activeEnergyUse;
+    }
+
+    public int getInactiveEnergyUse() {
+        return inactiveEnergyUse;
+    }
+}
---
+++ src/model/Manufacturers.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Manufacturers extends Infrastructure {
+    // Equipment and material costs to build one manufacturer
+    private static final int RESOURCE_BUILD_COST = 10;
+    private static final int EQUIPMENT_BUILD_COST = 10;
+
+    // Chance per million per day to breakdown
+    private static final int ACTIVE_BREAKDOWN_CHANCE = 100;
+    private static final int INACTIVE_BREAKDOWN_CHANCE = 1;
+
+    // Equipment and material costs to repair one manufacturer
+    private static final int RESOURCE_REPAIR_COST = 1;
+    private static final int EQUIPMENT_REPAIR_COST = 3;
+
+    // Energy use per unit
+    private static final int ACTIVE_ENERGY_USE = 1;
+    private static final int INACTIVE_ENERGY_USE = 0;
+
+    public Manufacturers() {
+        super(RESOURCE_BUILD_COST, EQUIPMENT_BUILD_COST,
+                ACTIVE_BREAKDOWN_CHANCE, INACTIVE_BREAKDOWN_CHANCE,
+                RESOURCE_REPAIR_COST, EQUIPMENT_REPAIR_COST,
+                ACTIVE_ENERGY_USE, INACTIVE_ENERGY_USE);
+    }
+}
