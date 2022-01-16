03bffce998904ae393c9002343bb3240fa7447ed
Linus Moreau <linus.moreau@gmail.com>
2022-01-15 17:38:52 -0800

    Rename money to funds; change rocket/pod signature

--- src/model/Earth.java
+++ src/model/Earth.java
@@ -2,22 +2,28 @@ package model;
 
 public class Earth {
     public static Earth earth;
-    private long money;
+    private long funds;
     private long rockets;
     private long receptors;
+
+    private static int RESOURCE_COST = 1;
+    private static int EQUIPMENT_COST = 2;
+    private static int MECHANIC_COST = 10;
+    private static int ROCKET_CAPACITY = 100;
+    private static int POD_CAPACITY = 50;
 
     public void generateEarth() {
         earth = new Earth();
     }
 
     private Earth() {
-        money = 0;
+        funds = 0;
         rockets = 0;
         receptors = 0;
     }
 
-    public long getMoney() {
-        return money;
+    public long getFunds() {
+        return funds;
     }
 
     public long getRockets() {
@@ -36,15 +42,15 @@ public class Earth {
         // stub
     }
 
-    public void launchRocket() {
+    public void launchRocket(int resources, int equipment, int mechanics) throws IllegalArgumentException {
         // stub
     }
 
-    public void launchPod(){
+    public void launchPod(int resources, int equipment, int mechanics) throws IllegalArgumentException {
         // stub
     }
 
     public void receiveFunding(long funding) {
-        money += funding;
+        funds += funding;
     }
 }
